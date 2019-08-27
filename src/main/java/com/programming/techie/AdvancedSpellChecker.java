8b94ad4e72fe47980013f24ee6feda32c50d6d9b
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2019-08-27 22:59:00 +0200

    Added log statement inside AdvancedSpellChecker

--- src/main/java/com/programming/techie/AdvancedSpellChecker.java
+++ src/main/java/com/programming/techie/AdvancedSpellChecker.java
@@ -7,6 +7,7 @@ public class AdvancedSpellChecker implements SpellChecker {
             // Determining the Language of the email Message
             // Check grammatical errors in the email Message
             // Check spellings in the email Message
+            System.out.println("Checking Spelling using Advanced Spell Checker..");
             System.out.println("Spell Checking Completed!!");
         } else {
             throw new RuntimeException("An exception occurred while checking Spelling");
