b0389bb98b5e0d516929220a840ebf381110cf43
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2019-08-31 01:40:18 +0200

    Code Changes for Video # 3 - Using Spring XML Configuration

--- src/main/java/com/programming/techie/BasicSpellChecker.java
+++ src/main/java/com/programming/techie/BasicSpellChecker.java
@@ -4,7 +4,7 @@ class BasicSpellChecker implements SpellChecker {
 
     public void checkSpelling(String emailMessage) {
         if (emailMessage != null) {
-            System.out.println("Checking spelling...");
+            System.out.println("Checking spelling using BasicSpellChecker...");
             System.out.println("Spell Checking Completed!!");
         } else {
             throw new RuntimeException("An exception occurred " +
--- src/main/java/com/programming/techie/EmailApplication.java
+++ src/main/java/com/programming/techie/EmailApplication.java
@@ -1,8 +1,13 @@
 package com.programming.techie;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class EmailApplication {
     public static void main(String[] args) {
-        EmailClient emailClient = new EmailClient(new BasicSpellChecker());
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("beans.xml");
+        EmailClient emailClient = applicationContext.getBean("emailClient", EmailClient.class);
         emailClient.sendEmail("Hey, " +
                 "This is my first email message.");
         emailClient.sendEmail("Hey, " +
