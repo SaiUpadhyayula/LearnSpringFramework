d68e47d0b36c3bdd109a9b7c1956a0872d8c872a
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2019-09-07 21:12:25 +0200

    Part 7 - Qualifier Annotation

--- src/main/java/com/programming/techie/EmailClient.java
+++ src/main/java/com/programming/techie/EmailClient.java
@@ -1,23 +1,14 @@
 package com.programming.techie;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 class EmailClient {
-    private SpellChecker spellChecker;
-
-    EmailClient() {
-    }
-
-    public SpellChecker getSpellChecker() {
-        return spellChecker;
-    }
-
     @Autowired
-    public void setSpellChecker(SpellChecker spellChecker) {
-        this.spellChecker = spellChecker;
-    }
+    @Qualifier("advancedSpellChecker")
+    private SpellChecker spellChecker;
 
     void sendEmail(String emailMessage) {
         spellChecker.checkSpelling(emailMessage);
